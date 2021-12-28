ef573217f15828cf114b235aca450db7fbea738a
Ricardo Arruda Sowek <ricardo.sowek@gmail.com>
2021-12-27 22:28:57 -0300

    byobu autostart

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -379,6 +379,13 @@ screens = [
     ),
 ]
 
+# Drag floating layouts.
+mouse = [
+    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
+#    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+#    Click([mod], "Button2", lazy.window.bring_to_front()),
+]
+
 dgroups_key_binder = None
 dgroups_app_rules = []  # type: List
 follow_mouse_focus = True
