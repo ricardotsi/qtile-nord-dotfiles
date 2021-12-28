2ed0f1e71d67adc95a71b70923f138467710e752
Ricardo Arruda Sowek <ricardo.sowek@gmail.com>
2021-12-27 21:25:37 -0300

    requirements

--- .config/fish/functions/ifpr.fish
+++ .config/fish/functions/ifpr.fish
@@ -1,3 +1,3 @@
-function ifpr --wraps='sudo openvpn Downloads/gwcwb-UDP4-1194-infcuritiba-config.ovpn' --wraps='printf infcuritiba | sudo openvpn Downloads/gwcwb-UDP4-1194-infcuritiba-config.ovpn' --wraps=' sudo openvpn Downloads/gwcwb-UDP4-1194-infcuritiba-config.ovpn' --description 'alias ifpr  sudo openvpn Downloads/gwcwb-UDP4-1194-infcuritiba-config.ovpn'
-   sudo openvpn Downloads/gwcwb-UDP4-1194-infcuritiba-config.ovpn $argv; 
+function ifpr --wraps='sudo openvpn Downloads/gwcwb-UDP4-1194-infcuritiba-config.ovpn' --wraps='printf infcuritiba | sudo openvpn Downloads/gwcwb-UDP4-1194-infcuritiba-config.ovpn' --wraps=' sudo openvpn Downloads/gwcwb-UDP4-1194-infcuritiba-config.ovpn' --wraps=' sudo openvpn Documents/gwcwb-UDP4-1194-infcuritiba-config.ovpn' --description 'alias ifpr  sudo openvpn Documents/gwcwb-UDP4-1194-infcuritiba-config.ovpn'
+   sudo openvpn Documents/gwcwb-UDP4-1194-infcuritiba-config.ovpn $argv; 
 end
