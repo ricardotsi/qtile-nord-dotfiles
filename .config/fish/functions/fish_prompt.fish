12f24d8d44f9d737859658011bc12b742413523e
Ricardo Arruda Sowek <ricardo.sowek@gmail.com>
2021-12-29 10:28:11 -0300

    prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -10,7 +10,7 @@ function fish_prompt
         echo -n (set_color red)'# '
     end
     
-    echo -n (set_color cyan)░▒▓(set_color -b cyan)(set_color "#86007d")" $USER@$hostname "(set_color -b normal)(set_color cyan)(set_color -b "04a0db")" "(prompt_pwd)"/ "(set_color -b "005b7c")(set_color "04a0db")" "(set_color white)(date "+%T")" "(set_color -b normal)(set_color "005b7c")""\n
+    echo -n (set_color "#88c0d0")░▒▓(set_color -b "#88c0d0")(set_color "#005b7c")" $USER@$hostname "(set_color -b normal)(set_color "#88c0d0")(set_color -b "04a0db")" "(set_color "#eceff4")(prompt_pwd)"/ "(set_color -b "005b7c")(set_color "04a0db")" "(set_color white)(date "+%T")" "(set_color -b normal)(set_color "005b7c")""\n
     echo -n (set_color "#e50000")'❯'(set_color "#ff8d00")'❯'(set_color "ffee00")'❯'(set_color "#028121")'❯'(set_color "#004cff")'❯'(set_color "#760088")'❯ '
     set_color normal
 end
